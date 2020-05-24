65a4a0d9a3f5f3515726b7f89bdc145c12577317
{RezaParvin} <{rezaparvin338@gmail.com}>
2020-05-24 15:28:45 +0430

    update logo and delete redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,16 @@ import { applyMiddleware, createStore, compose } from "redux";
 import rootReducer from "./reducers/index";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const middleware = [logger];
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
 
 export const store = createStore(
   rootReducer,
